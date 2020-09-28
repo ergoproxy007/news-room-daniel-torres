8694b32883c36a54f441459d9e5bc52257700e1a
Ing. Daniel Torres Ruiz <42307100+ergoproxy007@users.noreply.github.com>
2020-09-27 21:32:59 -0500

    GRAL: estructura proyecto 2

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ContenedorPrincipal from './redux/components/ContenedorPrincipal';
+
+function App() {
+  return (
+    <ContenedorPrincipal />
+  );
+}
+
+export default App;
---
+++ src/redux/components/ContenedorPrincipal/ContenedorPrincipal.tsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import "semantic-ui-css/semantic.min.css";
+
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility
+} from "semantic-ui-react";
+
+type ContenedorPrincipalProps = {
+  //
+};
+
+class ContenedorPrincipal extends Component<ContenedorPrincipalProps, any> {
+  state = {fixed: true};
+  
+  public render() {
+    const { children } = this.props;
+    const { fixed } = this.state;
+
+    return (
+      <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+        <Visibility
+          once={false}
+        >
+          <Segment
+            inverted
+            textAlign="center"
+            style={{ minHeight: 700, padding: "1em 0em" }}
+            vertical
+          >
+            <Menu
+              fixed={fixed ? "top" : "left"}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size="large"
+            >
+              <Container>
+                <Menu.Item as="a" active>
+                  Home
+                </Menu.Item>
+                <Menu.Item as="a">NUEVO DANIEL</Menu.Item>
+                <Menu.Item as="a">Work</Menu.Item>
+                <Menu.Item as="a">Company</Menu.Item>
+                <Menu.Item as="a">Careers</Menu.Item>
+                <Menu.Item position="right">
+                  <Button as="a" inverted={!fixed}>
+                    Log in
+                  </Button>
+                  <Button
+                    as="a"
+                    inverted={!fixed}
+                    primary={fixed}
+                    style={{ marginLeft: "0.5em" }}
+                  >
+                    Sign Up
+                  </Button>
+                </Menu.Item>
+              </Container>
+            </Menu>
+          </Segment>
+        </Visibility>
+
+        {children}
+      </Responsive>
+    );
+  }
+}
+
+export default ContenedorPrincipal;
